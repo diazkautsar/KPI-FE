8e3dd4ba68ef90765701929cc71f27105bcabfcd
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-25 13:21:09 +0700

    service auth

---
+++ src/services/auth.ts
@@ -0,0 +1,30 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import axiosInstance from '../libs/axios';
+import { constructErrorResponse } from '../helpers';
+import { BaseInterfaceResponse, LoginResponseInterface, ConstructErrorResponseInterface } from '../types';
+
+export const login = async (
+    username: string,
+    password: string
+): Promise<LoginResponseInterface | ConstructErrorResponseInterface> => {
+    try {
+        const response = await axiosInstance({
+            method: 'post',
+            url: '/auth/signin',
+            data: {
+                username_email: username,
+                password,
+            },
+        });
+
+        return {
+            success: response.data.success,
+            statusCode: response.data.statusCode,
+            message: response.data.message,
+            messageTitle: response.data.messageTitle,
+            data: response.data.data,
+        };
+    } catch (error) {
+        return constructErrorResponse(error);
+    }
+};
