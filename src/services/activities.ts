d7a4e3d0b04027ce15154df4b0259ad0bf0064f6
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-26 20:17:45 +0700

    add activities

--- src/services/activities.ts
+++ src/services/activities.ts
@@ -20,3 +20,28 @@ export const getActivities = async (): Promise<LoginResponseInterface | Construc
         return constructErrorResponse(error);
     }
 };
+
+export const addActivity = async (data: {
+    name: string;
+    description: string;
+    media_type: string;
+    media_url: string;
+}): Promise<LoginResponseInterface | ConstructErrorResponseInterface> => {
+    try {
+        const response = await axiosInstance({
+            method: 'post',
+            url: '/activity',
+            data,
+        });
+
+        return {
+            success: response.data.success,
+            statusCode: response.data.statusCode,
+            message: response.data.message,
+            messageTitle: response.data.messageTitle,
+            data: response.data.data,
+        };
+    } catch (error) {
+        return constructErrorResponse(error);
+    }
+};
