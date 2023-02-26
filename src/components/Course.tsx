c93f71f14a830a9687ab6176402b0a224723609c
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-26 15:09:00 +0700

    course

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { getAllCourses } from '../services/course';
 
 const CoursePage: React.FC = () => {
-    const [courses, setCourses] = React.useState([]);
+    const [courses, setCourses] = React.useState<{[K: string]:any}[]>([]);
     const navigate = useNavigate();
 
     React.useEffect(() => {
@@ -30,7 +30,60 @@ const CoursePage: React.FC = () => {
                         </Button>
                     </div>
                 </div>
-                <div className="d-flex flex-wrap justify-content-center mt-5">{JSON.stringify(courses)}</div>
+                <div className="d-flex flex-wrap justify-content-center mt-5">
+                    {courses.map((item, index) => {
+                        return (
+                            <Card key={index} style={{ width: '18rem', margin: '1rem 1rem' }}>
+                                <Card.Img variant="top" src={item.cover_image} alt="course-logo" style={{ height: '10rem' }} />
+                                <Card.Body>
+                                    <div className="text-center">
+                                        <div>
+                                            {' '}
+                                            <Card.Title>{item.name}</Card.Title>{' '}
+                                        </div>
+
+                                        <div>
+                                            <Card.Text>
+                                                { item.description }
+                                            </Card.Text>
+                                        </div>
+
+                                        <div className='mt-3'>
+                                            <div className='mb-1'> Keyword: </div>
+                                            <Card.Text>
+                                                { item.keyword }
+                                            </Card.Text>
+                                        </div>
+
+                                        <div className='mt-3'>
+                                            <div className='mb-1'> Modules: </div>
+                                            <Card.Text>
+                                                { item.modules.map((item: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }, index: React.Key | null | undefined) => {
+                                                    return (
+                                                        <React.Fragment key={index}>
+                                                            <div> {item.name} </div>
+                                                        </React.Fragment>
+                                                    )
+                                                }) }
+                                            </Card.Text>
+                                        </div>
+
+                                        {/* <div>
+                                            {' '}
+                                            <Button
+                                                onClick={() => goToDetail(item.link)}
+                                                variant="primary"
+                                                style={{ marginTop: '2rem' }}
+                                            >
+                                                Detail
+                                            </Button>{' '}
+                                        </div> */}
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        );
+                    })}
+                </div>
             </div>
         </React.Fragment>
     );
