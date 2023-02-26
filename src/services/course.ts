8eac5eb0a844770b5057fee89bea516ccdfae389
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-26 14:48:05 +0700

    add course

--- src/services/course.ts
+++ src/services/course.ts
@@ -20,3 +20,29 @@ export const getAllCourses = async (): Promise<LoginResponseInterface | Construc
         return constructErrorResponse(error);
     }
 };
+
+export const addCourse = async (data: {
+    name: string;
+    description: string;
+    cover_image: string;
+    keyword?: string;
+    modules: string[];
+}): Promise<LoginResponseInterface | ConstructErrorResponseInterface> => {
+    try {
+        const response = await axiosInstance({
+            method: 'post',
+            url: '/course',
+            data,
+        });
+
+        return {
+            success: response.data.success,
+            statusCode: response.data.statusCode,
+            message: response.data.message,
+            messageTitle: response.data.messageTitle,
+            data: response.data.data,
+        };
+    } catch (error) {
+        return constructErrorResponse(error);
+    }
+};
