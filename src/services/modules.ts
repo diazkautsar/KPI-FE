105e37249e288607cf09e4dd85715de915fe2082
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-26 18:40:01 +0700

    add modules

--- src/services/modules.ts
+++ src/services/modules.ts
@@ -20,3 +20,23 @@ export const getModules = async (): Promise<LoginResponseInterface | ConstructEr
         return constructErrorResponse(error);
     }
 };
+
+export const addModules = async (data: { name: string; description: string; activities: string[] }) => {
+    try {
+        const response = await axiosInstance({
+            method: 'post',
+            url: '/module',
+            data,
+        });
+
+        return {
+            success: response.data.success,
+            statusCode: response.data.statusCode,
+            message: response.data.message,
+            messageTitle: response.data.messageTitle,
+            data: response.data.data,
+        };
+    } catch (error) {
+        return constructErrorResponse(error);
+    }
+};
