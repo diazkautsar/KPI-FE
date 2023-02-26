1639af546a4ac710efa73de4405c089dd0f82ae7
DIAZ KAUTSAR <diazkautsar77@gmail.com>
2023-02-26 14:26:24 +0700

    get all course

---
+++ src/services/course.ts
@@ -0,0 +1,22 @@
+import axiosInstance from '../libs/axios';
+import { constructErrorResponse } from '../helpers';
+import { LoginResponseInterface, ConstructErrorResponseInterface } from '../types';
+
+export const getAllCourses = async (): Promise<LoginResponseInterface | ConstructErrorResponseInterface> => {
+    try {
+        const response = await axiosInstance({
+            method: 'get',
+            url: '/course',
+        });
+
+        return {
+            success: response.data.success,
+            statusCode: response.data.statusCode,
+            message: response.data.message,
+            messageTitle: response.data.messageTitle,
+            data: response.data.data,
+        };
+    } catch (error) {
+        return constructErrorResponse(error);
+    }
+};
